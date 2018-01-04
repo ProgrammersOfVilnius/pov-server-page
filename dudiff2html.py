26c6edb9a5c10712031dd2ce3072ee3076b14297
Marius Gedminas <marius@gedmin.as>
2018-01-04 20:31:03 +0200

    Avoid inline script in /du/diff

--- dudiff2html.py
+++ dudiff2html.py
@@ -134,49 +134,7 @@ dudiff_template = Template(textwrap.dedent('''
     % endfor
         </style>
 
-        <script type="text/javascript">
-          function by_delta(row) {
-            return parseInt(row.cells[0].dataset.size);
-          }
-          function by_path(row) {
-            return row.cells[1].innerText;
-          }
-          function sort(key) {
-            var table = document.getElementById("du-diff");
-            var tbody = table.tBodies[0];
-            var rows = tbody.rows;
-            var nrows = tbody.rows.length;
-            var arr = new Array();
-            var i;
-            tbody.className = 'sorting';
-            for (i = 0; i < nrows; i++) {
-              var row = rows[i];
-              arr[i] = [key(row), row.outerHTML, i];
-            };
-            arr.sort(function(a, b) {
-              return (a[0] < b[0] ? -1 : a[0] > b[0] ? 1 :
-                      a[2] < b[2] ? -1 : a[2] > b[2] ? 1 : 0);
-            });
-            for (i = 0; i < nrows; i++) {
-              arr[i] = arr[i][1];
-            }
-            tbody.innerHTML = arr.join('');
-            tbody.className = '';
-          }
-          function limit(depth) {
-            var table = document.getElementById("du-diff");
-            var i;
-            table.className = "du-diff table table-hover limit-" + depth;
-            for (i = 1; i < ${max_depth + 1}; i++) {
-              var btn = document.getElementById('depth-btn-' + i);
-              if (i == depth) {
-                btn.className = "btn btn-primary";
-              } else {
-                btn.className = "btn btn-default";
-              }
-            }
-          }
-        </script>
+        <script src="/static/js/dudiff.js"></script>
       </head>
       <body>
         <h1>du-diff for ${location} <small>${old} to ${new}</small></h1>
---
+++ static/js/dudiff.js
@@ -0,0 +1,40 @@
+function by_delta(row) {
+  return parseInt(row.cells[0].dataset.size);
+}
+function by_path(row) {
+  return row.cells[1].innerText;
+}
+function sort(key) {
+  var table = document.getElementById("du-diff");
+  var tbody = table.tBodies[0];
+  var rows = tbody.rows;
+  var nrows = tbody.rows.length;
+  var arr = new Array();
+  var i;
+  tbody.className = 'sorting';
+  for (i = 0; i < nrows; i++) {
+    var row = rows[i];
+    arr[i] = [key(row), row.outerHTML, i];
+  };
+  arr.sort(function(a, b) {
+    return (a[0] < b[0] ? -1 : a[0] > b[0] ? 1 :
+            a[2] < b[2] ? -1 : a[2] > b[2] ? 1 : 0);
+  });
+  for (i = 0; i < nrows; i++) {
+    arr[i] = arr[i][1];
+  }
+  tbody.innerHTML = arr.join('');
+  tbody.className = '';
+}
+function limit(depth) {
+  var table = document.getElementById("du-diff");
+  var i, btn;
+  table.className = "du-diff table table-hover limit-" + depth;
+  for (i = 1; (btn = document.getElementById('depth-btn-' + i)); i++) {
+    if (i == depth) {
+      btn.className = "btn btn-primary";
+    } else {
+      btn.className = "btn btn-default";
+    }
+  }
+}
